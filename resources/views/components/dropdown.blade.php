a50186ec8f4dae890aa237ab182201f605176ba9
Michael Ubaque <maikolubaque@gmail.com>
2025-05-23 12:43:45 -0500

    fix nav

--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -2,9 +2,9 @@
 
 @php
 $alignmentClasses = match ($align) {
-    'left' => 'ltr:origin-top-left rtl:origin-top-right start-0',
+    'left' => 'ltr:origin-top-left rtl:origin-top-right sm:start-0',
     'top' => 'origin-top',
-    default => 'ltr:origin-top-right rtl:origin-top-left end-0',
+    default => 'ltr:origin-top-right rtl:origin-top-left sm:end-0',
 };
 
 $width = match ($width) {
@@ -25,7 +25,7 @@ $width = match ($width) {
             x-transition:leave="transition ease-in duration-75"
             x-transition:leave-start="opacity-100 scale-100"
             x-transition:leave-end="opacity-0 scale-95"
-            class="absolute z-50 mt-2 {{ $width }} rounded-md shadow-lg {{ $alignmentClasses }}"
+            class="absolute left-4 sm:left-auto z-50 mt-2 {{ $width }} rounded-md shadow-lg {{ $alignmentClasses }}"
             style="display: none;"
             @click="open = false">
         <div class="rounded-md ring-1 ring-black ring-opacity-5 {{ $contentClasses }}">
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -18,18 +18,18 @@
                 </div>
 
                 <div class="hidden sm:flex sm:items-center sm:ms-6">
-                    <x-dropdown align="right" width="48">
+                    <x-dropdown align="left" width="48">
                         <x-slot name="trigger">
-                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
-                            <div>Configuración</div>
+                            <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
+                                <div>Configuración</div>
 
-                            <div class="ms-1">
-                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                                </svg>
-                            </div>
-                        </button>
-                    </x-slot>
+                                <div class="ms-1">
+                                    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                        <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                    </svg>
+                                </div>
+                            </button>
+                        </x-slot>
                         <x-slot name="content">
                             <x-dropdown-link :href="route('rector.edit')">
                                 Rector
@@ -104,8 +104,8 @@
                 <x-responsive-nav-link :href="route('profile.edit')">
                     {{ __('Profile') }}
                 </x-responsive-nav-link>
-                 <x-dropdown align="right" width="48">
-                        <x-slot name="trigger">
+                <x-dropdown align="right" width="48">
+                    <x-slot name="trigger">
                         <button class="inline-flex items-center px-4 py-2 border border-transparent leading-4 font-medium rounded-md text-gray-600 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
                             <div>Configuración</div>
 
@@ -116,12 +116,12 @@
                             </div>
                         </button>
                     </x-slot>
-                        <x-slot name="content">
-                            <x-dropdown-link :href="route('rector.edit')">
-                                Rector
-                            </x-dropdown-link>
-                        </x-slot>
-                    </x-dropdown>
+                    <x-slot name="content">
+                        <x-dropdown-link :href="route('rector.edit')">
+                            Rector
+                        </x-dropdown-link>
+                    </x-slot>
+                </x-dropdown>
 
                 <!-- Authentication -->
                 <form method="POST" action="{{ route('logout') }}">
