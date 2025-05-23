9407c6325215fb5949988bd3bbd332dd21aa6aba
Michael Ubaque <maikolubaque@gmail.com>
2025-05-23 09:04:26 -0500

    add rector to responsive navbar

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -17,7 +17,7 @@
                     </x-nav-link>
                 </div>
 
-                <div class="hidden sm:flex sm:items-center sm:ms-6">
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-dropdown align="right" width="48">
                         <x-slot name="trigger">
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
@@ -103,6 +103,9 @@
             <div class="mt-3 space-y-1">
                 <x-responsive-nav-link :href="route('profile.edit')">
                     {{ __('Profile') }}
+                </x-responsive-nav-link>
+                <x-responsive-nav-link :href="route('rector.edit')">
+                    Configuración
                 </x-responsive-nav-link>
 
                 <!-- Authentication -->
